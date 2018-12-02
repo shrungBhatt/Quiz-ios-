b702a0eb70aac21abc176480a0852b24b9b71406
shrungbhatt <shrungbhatt@gmail.com>
2018-12-02 22:10:43 +0530

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Shrung Bhatt on 02/12/18.
+//  Copyright © 2018 Shrung Bhatt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
