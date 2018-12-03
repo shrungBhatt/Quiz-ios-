161bdc650f1f09b1cad2b71daf3b9f81e062b317
shrungbhatt <shrungbhatt@gmail.com>
2018-12-03 07:23:08 +0530

    Completed till page 23

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var questionLabel: UILabel!;
+    @IBOutlet var answerLabel: UILabel!;
+    
+    let questions: [String] = [
+        "Which is the tallest statue in the World?",
+        "Which is the longest river in the world",
+        "Which is the tallest building in the world",
+        "What is the capital of vermont"
+    ];
+    
+    let answers: [String] = [
+        "Statue of Unity",
+        "Nile",
+        "Burj Khalifa",
+        "Montpelier"
+    ];
+    
+    var currentQuestionIndex: Int = 0;
+    
+    @IBAction func showNextQuestion(_sender: UIButton){
+        currentQuestionIndex += 1;
+        
+        if(currentQuestionIndex==questions.count){
+            currentQuestionIndex = 0;
+        }
+        
+        let question: String = questions[currentQuestionIndex];
+        questionLabel.text = question;
+        answerLabel.text = "???";
+        
     }
 
+    @IBAction func showAnswer(_sender: UIButton){
+        let answer: String = answers[currentQuestionIndex];
+        answerLabel.text = answer;
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        questionLabel.text = questions[currentQuestionIndex];
+        answerLabel.text = "???";
+    }
 
 }
 
